f9b01c7be74e059162aa476fa0281e505bf3fe3d
NaileeNesrullayeva <6W92Y2H@code.edu.az>
2024-11-17 06:27:00 -0800

    first

---
+++ main.js
@@ -0,0 +1,59 @@
+// function test(){
+//     console.log("bes saniye bitdi");
+
+// }
+// setTimeout(()=>{
+//     test()
+// },5000)
+
+// --------------------------------------------
+
+// setInterval(()=>{
+
+// })
+
+// --------------------------------------
+
+// let n=65.4345
+// console.log(Math.round(n));
+// ----------------------------------------------
+
+
+// function random(){
+//     var n=Math.round(Math.random()*(10-0) +1) 
+//     return n
+// }
+// console.log(random());
+
+// ----------------------------------------------------
+
+
+// function date(){
+//     var dt=new Date()
+//     return dt.getFullYear
+// }
+// console.log(date());
+
+// --------------------------------------------------------
+
+
+
+// function Date(){
+//     var dt=new Date()
+//     date=dt.getDate()
+//     month=dt.getMonth()
+//     year=dt.getFullYear()
+//     return `${date} ${month} ${year}`
+// }
+// console.log(Date())
+
+
+
+// ---------------------------------------------------
+
+// function newYear(){
+//     var dt=new Date('15.11.2025')
+//     return dt.getDay()
+    
+// }
+// console.log(newYear())
